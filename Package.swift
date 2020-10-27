9f0c1a2fb577ac87c2b61cac02cd29700c6fc87a
Ron Regev <ron.regev@gmail.com>
2020-10-28 01:20:53 +0200

    Updated SPM link

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PDFFreedraw",
-            url: "http://www.ronregev.com/ScoreWizard/PDFFreedraw.xcframework.zip",
+            url: "https://drive.google.com/uc?export=download&id=19n8_BNOau_IpyCOfcLdKkUQcghPXHChd",
             checksum: "34ab171fed4421f2ad29f2475586f36a20667a5b4bcf514ed6e4d629a1a2481d"
         )
     ]
