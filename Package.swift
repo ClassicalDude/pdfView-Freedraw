c08cb08b73ce786bd332626e6d6584c4f6759b31
Ron Regev <ron.regev@gmail.com>
2020-10-28 01:29:52 +0200

    Updated package checksum

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
         .binaryTarget(
             name: "PDFFreedraw",
             url: "https://www.dropbox.com/s/jo792e00gmqw6rt/PDFFreedraw.xcframework.zip",
-            checksum: "34ab171fed4421f2ad29f2475586f36a20667a5b4bcf514ed6e4d629a1a2481d"
+            checksum: "90bfe69494ebe24181d025c2ec738c42ea123738ba37e4ec2234980bad32973b"
         )
     ]
 )
+
